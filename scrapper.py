32489646fe5ade1a6868e088906fa015efc69323
Alexandre <mail.alefarias@gmail.com>
2018-02-21 14:20:34 -0300

    add quantity parameter of images on get_images_of function

--- scrapper.py
+++ scrapper.py
@@ -5,49 +5,56 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time, os
 
-def download_images(class_name, image_src_list):
+def download_images(class_name, image_src_list, quant_images):
+    """
+    get a list of images and use urlretrieve to
+    try download every image in hte list
+    """
+    # IMPORTANT!: if the class name already exists the function will not download the images
     os.mkdir(class_name)
+    
     images_downloaded = 0
     for index, image_src in enumerate(image_src_list):
         try:
             urlretrieve(image_src, class_name+"/"+class_name+str(index)+'.jpg')
-            print('download [OK]:', image_src)
+            print('download [OK]:', image_src[:10]+'...')
             images_downloaded += 1
         except:
-            print('download [FAILED]:', image_src)
+            print('download [FAILED]:', image_src[:10]+'...')
+        if index == quant_images - 1: break
     print(images_downloaded, 'images downloaded from the class', class_name, 'check your class folder.')
                 
-def get_images_of(class_name):
+def get_images_of(class_name, quant_images):
+    """
+    receive a class name that will be used for
+    search images using google images tool
+    """
     url = "https://www.google.com.br/search?q="+class_name+"&prmd=inv&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj3mdzbnrXZAhWQuFMKHY7DAbYQ_AUIESgB#imgrc=_"
     path_webdriver = "C:\\webdrivers\\chromedriver.exe"
     driver = webdriver.Chrome(path_webdriver)
     driver.get(url)
-
+    
     print("Get a coffe, while I search the images for you")
+
+    # IMPORTANT!: the scrolls are necessary to render more images on the screen for scrap
     jump_size = 200
     for i in range(20):
         base_jump = 1000
         driver.execute_script('window.scrollTo(0, '+str(base_jump + jump_size)+');')
         jump_size += 200
         time.sleep(1)
-        
+
     html = driver.page_source
     bsobj = BeautifulSoup(html, 'html5lib')
     image_list = bsobj.findAll('img',{'class':'rg_ic rg_i'})
 
     image_src_list = []
     for image in image_list:
-        try:
-            image_src_list.append(image.attrs['src'])
-        except:
-            print('failed to found the source of', image)
+        try: image_src_list.append(image.attrs['src'])
+        except: print('failed to found the source of', image[:10]+'...')
+        
     driver.close()
     print(len(image_src_list),'found, starting the download!')
-    download_images(class_name, image_src_list)
+    download_images(class_name, image_src_list, quant_images)
 
-message = """
-        GOOGLE IMAGES SCRAPPER
-        Initializing search...
-"""
-print(message)
-get_images_of('morango')
+get_images_of('da vinci', 15)
